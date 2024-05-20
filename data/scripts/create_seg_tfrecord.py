6d7d7fc5ee17123db8451326fab9a0ef309c8b3b
abhineet123 <asingh1@ualberta.ca>
2024-05-19 22:58:48 -0600

    minor fix

--- data/scripts/create_seg_tfrecord.py
+++ data/scripts/create_seg_tfrecord.py
@@ -517,22 +517,22 @@ def main():
         vid_infos=vid_infos,
     )
 
-    for idx, annotations_iter_ in tqdm(enumerate(annotations_iter), total=len(image_infos)):
-        create_tf_example(*annotations_iter_)
+    # for idx, annotations_iter_ in tqdm(enumerate(annotations_iter), total=len(image_infos)):
+    #     create_tf_example(*annotations_iter_)
 
-    # if params.stats_only:
-    #     for idx, annotations_iter_ in tqdm(enumerate(annotations_iter), total=len(image_infos)):
-    #             create_tf_example(*annotations_iter_)
-    # else:
-    #     tfrecord_pattern = os.path.join(output_path, 'shard')
-    #     tfrecord_lib.write_tf_record_dataset(
-    #         output_path=tfrecord_pattern,
-    #         annotation_iterator=annotations_iter,
-    #         process_func=create_tf_example,
-    #         num_shards=params.num_shards,
-    #         multiple_processes=params.n_proc,
-    #         iter_len=len(image_infos),
-    #     )
+    if params.stats_only:
+        for idx, annotations_iter_ in tqdm(enumerate(annotations_iter), total=len(image_infos)):
+                create_tf_example(*annotations_iter_)
+    else:
+        tfrecord_pattern = os.path.join(output_path, 'shard')
+        tfrecord_lib.write_tf_record_dataset(
+            output_path=tfrecord_pattern,
+            annotation_iterator=annotations_iter,
+            process_func=create_tf_example,
+            num_shards=params.num_shards,
+            multiple_processes=params.n_proc,
+            iter_len=len(image_infos),
+        )
     print(f'output_path: {output_path}')
 
     for method, metrics_ in metrics.items():
